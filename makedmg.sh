3d16ac3826d7f12d20c662b7cdcf78172d4e194c
Brenton Partridge <bapartridge@gmail.com>
2016-08-06 15:47:20 -0400

    Use local installation of appdmg

--- makedmg.sh
+++ makedmg.sh
@@ -4,5 +4,4 @@ PACKAGE_VERSION=$(grep -m1 version package.json | awk -F: '{ print $2 }' | sed '
 DEST="./dist/WhatsApp-$PACKAGE_VERSION.dmg"
 
 [ -f "$DEST" ] && rm "$DEST"
-[ $(node -p "require('appdmg/package.json').version") != "0.4.5" ] && npm install -g appdmg
-appdmg ./dmg.json "$DEST"
+./node_modules/.bin/appdmg ./dmg.json "$DEST"
